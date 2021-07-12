7b2804afa2fecbf5fdb1c0d31b11439bb92d6c32
roshanlee <roshan26bhagat@gmail.com>
2021-07-12 19:19:25 +0530

    Welcomt to snake ladder

---
+++ StartUC1.java
@@ -0,0 +1,10 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StartUC1 {
+	private final static Logger logger= Logger.getLogger("StartUC1");
+	public static void main(String[] args) {
+		logger.log(Level.INFO,"------Welcome To Snake And Ladder!!------  ");
+	GameInfoUC6.gameInfo();
+	}
+	}
